c1de0b96b8d591685f75af651bba5e8903f58b2d
Roman <romprogramist@gmail.com>
2021-01-11 19:01:24 +0300

    slider

--- js/slick.js
+++ js/slick.js
@@ -42,75 +42,10 @@ $(document).ready(function(){
                 settings: {                    
                     slidesToShow: 1
                 },
-            } 
-               
-            
-            // {
-            //     breakpoint: 1522,    
-            //     settings: {
-                    
-            //         slidesToShow: 3.67                                                      
-            //     }
-            // },
-            // {
-            //     breakpoint: 1257,    
-            //     settings: {
-                    
-            //         slidesToShow: 3
-            //     }
-            // },
-            // {
-            //     breakpoint: 1067,    
-            //     settings: {                    
-            //         slidesToShow: 2.33                                                 
-            //     }
-            // },
-            // {
-            //     breakpoint: 980,
-            //     settings: { 
-            //         slidesToShow: 1.65                                        
-            //     }
-            // },
-            // {
-            //     breakpoint: 885,    
-            //     settings: {  
-            //         slidesToShow: 1.67                           
-            //     }
-            // },
-            // {
-            //     breakpoint: 715,    
-            //     settings: {
-            //         slidesToShow: 1.69                                    
-            //     }
-            // },
+            }     
         ],       
     });
 
-
-
-    // function doCheck() {
-    //     const now = new Date();
-    //     if (now.getHours() !== 13) { // часы
-    //     timeout();
-    //     } else {
-    //     if (now.getMinutes() !== 59) { // минуты
-    //     timeout();
-    //     } else {
-    //     if (now.getSeconds() !== 57) { // секунды
-    //     timeout()
-    //     } else {
-    //     document.querySelector("#checkout > div.buttonContainer > button").click();
-    //     }
-    //     }
-    //     }
-    //     }
-    //     function timeout() {
-    //     setTimeout(() => {
-    //     doCheck();
-    //     }, 750)
-    //     }
-    //     doCheck();
-        
 
 
     $('.slider1').slick({
@@ -118,7 +53,6 @@ $(document).ready(function(){
         infinite: true,
         dots: true,
         slidesToShow: 1.7,
-        // centerPadding: '160px',
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/SliderArrowLeft.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/SliderArrowRight.svg"></button>',
@@ -137,8 +71,6 @@ $(document).ready(function(){
               },
           ]
     });
-
-    
   });
 
 
